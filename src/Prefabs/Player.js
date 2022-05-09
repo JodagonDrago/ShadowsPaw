e6ccdc0317192dcb390b6c639f52de2c3ec7dc24
Aaron Gonzales <agonz243@ucsc.edu>
2022-05-09 13:48:20 -0700

    Added 50px movement to player

--- src/Prefabs/Player.js
+++ src/Prefabs/Player.js
@@ -8,24 +8,58 @@ class Player extends Phaser.Physics.Arcade.Sprite { //made a physics object inst
       this.body.allowGravity = false; //no gravity cause it's top down
       scene.add.existing(this);
 
+      // Boolean to prevent movement if player is already moving
+      this.isMoving = false;
+
+      // Reserve scene as local variable
+      this.scene = scene;
+
     }
 
     update(){
         // movement using arcade physics and global variables in main.js
         // Check keyboard input for directional movement
-        if (cursors.left.isDown) {
+        if (cursors.left.isDown && !this.isMoving) {
             console.log('Left pushed');
+            this.isMoving = true;
+            this.x -= 50;
 
-        } else if (cursors.right.isDown) {
+            // Delay movement until player has moved one unit (50px)
+            this.delayMove();
+
+        } else if (cursors.right.isDown && !this.isMoving) {
             console.log('Right pushed');
+            this.isMoving = true;
+            this.x += 50;
 
-        } else if (cursors.up.isDown) {
+            // Delay movement until player has moved one unit (50px)
+            this.delayMove();
+
+        } else if (cursors.up.isDown && !this.isMoving) {
             console.log('Up pushed');
+            this.isMoving = true;
+            this.y -= 50;
 
-        } else if (cursors.down.isDown) {
+            // Delay movement until player has moved one unit (50px)
+            this.delayMove();
+
+        } else if (cursors.down.isDown && !this.isMoving) {
             console.log('Down pushed');
+            this.isMoving = true;
+            this.y += 50;
+
+            // Delay movement until player has moved one unit (50px)
+            this.delayMove();
+
         }
         
+    }
+
+    // A function for delaying movement so that player does not move several times a second
+    delayMove() {
+        this.scene.time.delayedCall(500, () => {
+            this.isMoving = false;
+        }, null, this);
     }
 
 }
